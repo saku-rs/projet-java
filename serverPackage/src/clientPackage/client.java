b35328b42ddbd1680d1dcaf4a35692516a540ef3
saku <rahmasanoun@gmail.com>
2025-10-06 15:13:01 +0100

    activite1.2

--- serverPackage/src/clientPackage/client.java
+++ serverPackage/src/clientPackage/client.java
@@ -1,17 +1,39 @@
 package clientPackage;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) throws Exception {
 try {
-            System.out.println("Je suis un client pas encore connecté…");
             Socket socket = new Socket("localhost", 1234);
-            System.out.println("Je suis un client connecté!");
+            System.out.println("Client connecté au serveur.");
 
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+
+            int x;
+            do {
+                System.out.print("Entrez un entier (0 pour quitter) : ");
+                x = scanner.nextInt();
+
+                out.writeInt(x); 
+
+                if (x != 0) {
+                    int result = in.readInt(); 
+                    System.out.println("Résultat reçu du serveur : " + result);
+                }
+
+            } while (x != 0);
+
+            System.out.println("Fin de la communication.");
+            in.close();
+            out.close();
             socket.close();
-            System.out.println("Connexion fermée.");
         } catch (IOException e) {
             e.printStackTrace();
     }
--- serverPackage/src/serverPackage/server.java
+++ serverPackage/src/serverPackage/server.java
@@ -9,15 +9,30 @@ public class server {
             System.out.println("Je suis un serveur en attente de la connexion d'un client...");
 
             Socket socket = serverSocket.accept();
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             System.out.println("Un client est connecté!");
+            int x;
+            do {
+                x = in.readInt(); 
+                System.out.println("Reçu du client: " + x);
+                if (x != 0) {
+                    int result = x * 5;
+                    out.writeInt(result);
+                    System.out.println("Résultat envoyé: " + result);
+                }
 
+            } while (x != 0);
+
+            System.out.println("Fin de la communication.");
+            in.close();
+            out.close();
             socket.close();
             serverSocket.close();
             System.out.println("Connexion fermée.");
         } catch (IOException e) {
             e.printStackTrace();
-        }
-       
+        }     
     }
 }
 
